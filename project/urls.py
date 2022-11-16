cdbcbb13ba04ec20b021ed13c5838eafcb83c57c
Nikola Panic <nikolapanic@outlook.fr>
2022-11-16 16:43:13 +0100

    fix home page

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
